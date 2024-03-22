86b84e58da5a3f36024eb492074001d517d06253
chrishenderson07 <chris.hend012@gmail.com>
2024-03-22 14:57:12 -0300

    fix: Buttons changed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,34 +5,9 @@ import GameTeaserHero from '@/components/banner/GameTeaserHero'
 
 import { getVideos } from '../api/services/entities/videos'
 import Image from 'next/image'
-// import { useState } from 'react'
 
 const Home = async () => {
-	// const [thumbnailUrl, setThumbnailUrl] = useState<string[]>([])
 	const videosData = await getVideos()
-
-	// const thumbnailsUrls = videosData.map((video) => {
-	// 	const url = fetchThumbnail(video)
-	// 	console.log(url)
-	// 	return url
-	// })
-
-	// const fetchData = async () => {
-	// 	try {
-	// 		const videos = await getVideos()
-	// 		const updatedVideosData = await Promise.all(
-	// 			videos.map(async (video) => {
-	// 				const thumbnailUrl = await fetchThumbnail(video)
-	// 				return { ...video, thumbnailUrl }
-	// 			}),
-	// 		)
-	// 		videosData = updatedVideosData
-	// 	} catch (error) {
-	// 		console.error('Erro ao buscar os vídeos:', error)
-	// 	}
-	// }
-
-	// fetchData()
 
 	function fetchThumbnail(videoData: any) {
 		try {
--- src/components/banner/GameTeaserHero.tsx
+++ src/components/banner/GameTeaserHero.tsx
@@ -14,7 +14,7 @@ export default function GameTeaserHero({ data, style }: Props) {
 			className="py-[26vw] max-h-[90vh] flex items-center relative gradient-bg"
 			style={style}>
 			<div className="z-50 wide-wrapping w-full ">
-				<div className="text-4xl font-extrabold uppercase text-skeleton-dark laptop:text-7xl">
+				<div className="text-4xl font-extrabold uppercase text-lightBackground laptop:text-7xl">
 					<span>{data.title}</span>
 					<div className="space-x-4">
 						<span>{data.description}</span>
@@ -23,8 +23,8 @@ export default function GameTeaserHero({ data, style }: Props) {
 
 				<div className="space-x-4 mt-8 ">
 					<Button
-						variant={'gradient'}
 						size={'hero'}
+						className="bg-primaryDetailsColor text-white rounded-lg accentHover"
 						asChild>
 						<Link
 							href={`video/${data.id}`}
@@ -36,6 +36,7 @@ export default function GameTeaserHero({ data, style }: Props) {
 					<Button
 						variant={'border'}
 						size={'hero'}
+						className=" text-white rounded-lg transition-all duration-300 hover:bg-lightBackground hover:text-primaryDetailsColor hover:border-lightBackground"
 						asChild>
 						<Link
 							href={'/'}
