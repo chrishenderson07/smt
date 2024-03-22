3a13ffd2c57082cf9d73641af96aa6eca53eba2a
chrishenderson07 <chris.hend012@gmail.com>
2024-03-22 11:30:04 -0300

    feat: initiate GameTeaserHero component with youtube thumbnail

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,24 @@
-const Home = () => {
+'use client'
+
+import HeroSlide from '@/components/carousel/HeroSlide'
+import GameTeaserHero from '@/components/banner/GameTeaserHero'
+
+import { fetchVideos, getVideos } from '../api/services/entities/videos'
+import { useState } from 'react'
+
+const Home = async () => {
+	const [thumbnailUrl, setThumbnailUrl] = useState('')
+
+	const videosData = await getVideos()
+	async function fetchThumbnail() {
+		// const response = await fetchVideos()
+	}
+
 	return (
 		<div className="">
-			<h1>Estou na home</h1>
+			<HeroSlide>
+				<GameTeaserHero data={videosData[0]} />
+			</HeroSlide>
 		</div>
 	)
 }
--- src/components/banner/GameTeaserHero.tsx
+++ src/components/banner/GameTeaserHero.tsx
@@ -1,38 +1,49 @@
 import { Video } from '@prisma/client'
-import React from 'react';
-import { Button } from '../ui/button';
-import Link from 'next/link';
-import pageIcons from '@/utils/pageIcons';
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import pageIcons from '@/utils/pageIcons'
 
-export default function GameTeaserHero({data}:{data:Video}) {
-  return (
-    <div className='py-[26vw] max-h-[90vh] flex items-center'>
-       <div className='wide-wrapping w-full'>
-          <div className='text-4xl font-extrabold uppercase text-skeleton-dark laptop:text-7xl'>
-           <span>JORDÂNIA</span>
-           <div className='space-x-4'>
-           <span className='drop-shadow-[0_1.2px_1.2px_rgba(242, 242, 242, 0.8)]'>VS</span>
-           <span>Coreia do Sul</span>
-           </div>
-          </div>
+interface Props {
+	data: Video
+}
 
-          <div className='space-x-4 mt-8'>
-            <Button  variant={"gradient"} size={"hero"} asChild>
-              <Link href={`video/${data.id}`} className='space-x-2'>
-                <pageIcons.PlayerFill/>
-                <span>Assista Agora</span>
-              </Link>
-            </Button>
-            <Button  variant={"border"} size={"hero"} asChild>
-              <Link href={"/"} className='space-x-2'>
-                <pageIcons.Calendar/>
-                <span>Calendário</span>
-              </Link>
-            </Button>
-          </div>
-        </div>
+export default function GameTeaserHero({ data }: Props) {
+	return (
+		<div
+			className={`py-[26vw] max-h-[90vh] flex items-center bg-cover bg-center`}>
+			<div className="wide-wrapping w-full">
+				<div className="text-4xl font-extrabold uppercase text-skeleton-dark laptop:text-7xl">
+					<span>{data.title}</span>
+					<div className="space-x-4">
+						<span>{data.description}</span>
+					</div>
+				</div>
 
-       </div>
-  
-  )
+				<div className="space-x-4 mt-8">
+					<Button
+						variant={'gradient'}
+						size={'hero'}
+						asChild>
+						<Link
+							href={`video/${data.id}`}
+							className="space-x-2">
+							<pageIcons.PlayerFill />
+							<span>Assista Agora</span>
+						</Link>
+					</Button>
+					<Button
+						variant={'border'}
+						size={'hero'}
+						asChild>
+						<Link
+							href={'/'}
+							className="space-x-2">
+							<pageIcons.Calendar />
+							<span>Calendário</span>
+						</Link>
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
