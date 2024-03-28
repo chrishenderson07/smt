0425ea907950942b192edcacccc446873678d41a
chrishenderson07 <chris.hend012@gmail.com>
2024-03-28 15:53:15 -0300

    fix: new test

--- src/components/carousel/SlideWrapper.tsx
+++ src/components/carousel/SlideWrapper.tsx
@@ -1,33 +1,48 @@
-import React from 'react';
-import { getCardFetchedConfig } from '@/app/api/services/service-config';
-import Slide from './Slide';
+import React from 'react'
+import { getCardFetchedConfig } from '@/app/api/services/service-config'
+import Slide from './Slide'
 
+export default async function SlideWrapper({
+	dataType,
+	existingData,
+	fetchDataProp,
+	cardProps,
+}: {
+	dataType: string
+	existingData?: any[]
+	fetchDataProp?: {}
+	cardProps?: {}
+}) {
+	let { datas = [], Component } = await getCardFetchedConfig(
+		dataType,
+		existingData,
+		{ ...fetchDataProp, take: 14 },
+	)
 
-export default async function SlideWrapper({ dataType, existingData, fetchDataProp, cardProps}
-  : { dataType: string, existingData?: any[], fetchDataProp?:{}, cardProps?:{}}) 
-  {
-  let { datas = [], Component } = await getCardFetchedConfig(dataType, existingData, {...fetchDataProp, take:14});
-
-  return (
-    <div className='wide-wrapping'>
-
-      {
-        (Array.isArray(datas) && datas?.length > 0) 
-        ? (
-             <>
-              <Slide 
-            configs={{
-             spaceBetween: 20,
-             slidesPerView: "auto",
-            }}>
-                {
-                  datas.map(data =><>{Component && <Component data={data} />}</>)
-                }
-              </Slide>
-             </>
-        ) 
-        : <></>
-      }
-    </div>
-  )
+	return (
+		<div className="wide-wrapping">
+			{Array.isArray(datas) && datas?.length > 0 ? (
+				<>
+					<Slide
+						configs={{
+							spaceBetween: 20,
+							slidesPerView: 'auto',
+						}}>
+						{datas.map((data) => (
+							<>
+								{Component && (
+									<Component
+										fixedMeasure
+										data={data}
+									/>
+								)}
+							</>
+						))}
+					</Slide>
+				</>
+			) : (
+				<></>
+			)}
+		</div>
+	)
 }
