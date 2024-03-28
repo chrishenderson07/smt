3d7d299437d84a09635adb3a620caab79e011b89
chrishenderson07 <chris.hend012@gmail.com>
2024-03-28 15:47:14 -0300

    fix: test deploy2

--- src/app/api/services/entities/tournament.ts
+++ src/app/api/services/entities/tournament.ts
@@ -1,67 +1,61 @@
-import { Tournament } from "@prisma/client";
-import prisma from "../../../../../prisma/client";
-import { Prisma } from "@prisma/client";
+// import { Tournament } from "@prisma/client";
+// import prisma from "../../../../../prisma/client";
+// import { Prisma } from "@prisma/client";
 
+// export interface ITournament{
+//     data: Tournament
+// }
 
-export interface ITournament{
-    data: Tournament
-}
+// export const tournamentIncludes = {
+//     arenas:{arenas:true},
+//     videos:{videos:true}
+// }
 
-export const tournamentIncludes = {
-    arenas:{arenas:true},
-    videos:{videos:true}
-}
+// export async function getTournamets(options?: Prisma.TournamentFindManyArgs) {
+//     return await prisma.tournament.findMany(options)
+//   }
 
+// export async function getTournamentById(arenaId: string, includes: {}) {
 
-export async function getTournamets(options?: Prisma.TournamentFindManyArgs) {
-    return await prisma.tournament.findMany(options)
-  }
+//     const tournament = await prisma.tournament.findUnique({
+//       where: { id: arenaId },
+//       include: includes
+//     });
 
-export async function getTournamentById(arenaId: string, includes: {}) {
+//     return tournament;
+// }
 
-    const tournament = await prisma.tournament.findUnique({
-      where: { id: arenaId },
-      include: includes
-    });
+// interface TournamentWithDate {
+//   date: Date;
+//   tournaments: Tournament[];
+// }
 
-    return tournament;
-}
+// export async function getTournamentsOrganizedByDate(options?: Prisma.TournamentFindManyArgs) {
+//   const tournaments = await prisma.tournament.findMany({
+//     ...options,
+//   });
 
+//   const organizedTournaments: TournamentWithDate[] = [];
 
+//   tournaments.forEach(tournament => {
+//     const tournamentDate = new Date(tournament?.date);
+//     const existingGroup = organizedTournaments.find(group => isSameDay(group.date, tournamentDate));
 
-interface TournamentWithDate {
-  date: Date;
-  tournaments: Tournament[];
-}
+//     if (existingGroup) {
+//       existingGroup.tournaments.push(tournament);
+//     } else {
+//       organizedTournaments.push({
+//         date: tournamentDate,
+//         tournaments: [tournament]
+//       });
+//     }
+//   });
 
-export async function getTournamentsOrganizedByDate(options?: Prisma.TournamentFindManyArgs) {
-  const tournaments = await prisma.tournament.findMany({
-    ...options, 
-  });
+//   return organizedTournaments ;
+// }
 
-   
-  const organizedTournaments: TournamentWithDate[] = [];
-
-  tournaments.forEach(tournament => {
-    const tournamentDate = new Date(tournament?.date);
-    const existingGroup = organizedTournaments.find(group => isSameDay(group.date, tournamentDate));
-
-    if (existingGroup) {
-      existingGroup.tournaments.push(tournament);
-    } else {
-      organizedTournaments.push({
-        date: tournamentDate,
-        tournaments: [tournament]
-      });
-    }
-  });
-
-  return organizedTournaments ;
-}
-
-function isSameDay(date1: Date, date2: Date): boolean {
-    return date1.getFullYear() === date2.getFullYear() &&
-      date1.getMonth() === date2.getMonth() &&
-      date1.getDate() === date2.getDate();
-  }
- 
+// function isSameDay(date1: Date, date2: Date): boolean {
+//     return date1.getFullYear() === date2.getFullYear() &&
+//       date1.getMonth() === date2.getMonth() &&
+//       date1.getDate() === date2.getDate();
+//   }
