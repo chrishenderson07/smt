5d205400dbb1222b58d73f0e8113243ece69e4ad
chrishenderson07 <chris.hend012@gmail.com>
2024-03-28 15:12:50 -0300

    fix: comment errors

--- src/components/list/SocialMediaList.tsx
+++ src/components/list/SocialMediaList.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { socialMediasArray } from '@/data/social-media-links';
-import Link from 'next/link';
+import React from 'react'
+// import { socialMediasArray } from '@/data/social-media-links';
+import Link from 'next/link'
 
-export default function SocialMediaList({className}: {className?: string}) {
-  return (
-    <ul className={className}>
-       {
+export default function SocialMediaList({ className }: { className?: string }) {
+	return (
+		<ul className={className}>
+			{/* {
           socialMediasArray?.map((media) => {
             return <li key={media?.label}><Link href={media.link}><media.Icon className="hover:text-detail-primary"/></Link></li>;
           })
-        }
-    </ul>
-  )
+        } */}
+		</ul>
+	)
 }
