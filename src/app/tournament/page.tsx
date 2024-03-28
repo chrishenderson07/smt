d582a2e8d483e083fa37e5c69b66ba15f7471ca8
chrishenderson07 <chris.hend012@gmail.com>
2024-03-28 15:42:19 -0300

    fix: test deploy

--- src/app/tournament/page.tsx
+++ src/app/tournament/page.tsx
@@ -1,9 +1,8 @@
 // import { getTournamets } from '@/app/api/services/entities/tournament'
-// import TournamentCalendar from '@/components/card/TournamentCalendar'
-// import { Tournament } from '@prisma/client'
-// import React from 'react'
-// import { Button } from '@/components/ui/button'
-// // import FilterSection from '@/app/components/filter/FilterSection'
+import TournamentCalendar from '@/components/card/TournamentCalendar'
+import { Tournament } from '@prisma/client'
+import { Button } from '@/components/ui/button'
+// import FilterSection from '@/app/components/filter/FilterSection'
 
 // function groupTournamentsByMonth(
 // 	tournaments: Tournament[],
@@ -35,39 +34,39 @@
 // 	}))
 // }
 
-// export default async function page() {
-// 	const tournaments = await getTournamets()
-// 	const tournamentByMonth = groupTournamentsByMonth(tournaments)
+export default async function page() {
+	// const tournaments = await getTournamets()
+	// const tournamentByMonth = groupTournamentsByMonth(tournaments)
 
-// 	console.log(tournamentByMonth[1])
+	// console.log(tournamentByMonth[1])
 
-// 	return (
-// 		<div>
-// 			<div className=" py-4 mb-8 sticky top-0 bg-skeleton-lessOpacity">
-// 				<div className="wide-wrapping flex-between-center ">
-// 					<h1>Calendário de torneios</h1>
-// 					{/* <FilterSection/> */}
-// 				</div>
-// 			</div>
-// 			<div className="wide-wrapping space-y-12">
-// 				{tournamentByMonth.map(({ month, tournaments }) => (
-// 					<div
-// 						key={month}
-// 						className="space-y-6 laptop:flex laptop:space-x-3">
-// 						<div className="w-[10%]">
-// 							<Button className="capitalize">{month}</Button>
-// 						</div>
-// 						<div className="grow space-y-6">
-// 							{tournaments.map((tournament) => (
-// 								<TournamentCalendar
-// 									key={tournament.id}
-// 									data={tournament}
-// 								/>
-// 							))}
-// 						</div>
-// 					</div>
-// 				))}
-// 			</div>
-// 		</div>
-// 	)
-// }
+	return (
+		<div>
+			<div className=" py-4 mb-8 sticky top-0 bg-skeleton-lessOpacity">
+				<div className="wide-wrapping flex-between-center ">
+					<h1>Calendário de torneios</h1>
+					{/* <FilterSection/> */}
+				</div>
+			</div>
+			<div className="wide-wrapping space-y-12">
+				{/* {tournamentByMonth.map(({ month, tournaments }) => (
+					<div
+						key={month}
+						className="space-y-6 laptop:flex laptop:space-x-3">
+						<div className="w-[10%]">
+							<Button className="capitalize">{month}</Button>
+						</div>
+						<div className="grow space-y-6">
+							{tournaments.map((tournament) => (
+								<TournamentCalendar
+									key={tournament.id}
+									data={tournament}
+								/>
+							))}
+						</div>
+					</div>
+				))} */}
+			</div>
+		</div>
+	)
+}
